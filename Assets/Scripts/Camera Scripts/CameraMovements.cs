f6ce0ebeba3641f4f51cd4c9c4cf13f7acd20aea
Anghofal <asutedjaww@gmail.com>
2024-06-03 22:41:43 +0700

    adding feature camera follow head

---
+++ Assets/Scripts/Camera Scripts/CameraMovements.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+public class CameraCenter : MonoBehaviour
+{
+    [Range(0.0f, 0.5f)]
+    [SerializeField] private float cameraSmoothSpeed;
+    [SerializeField] private Transform snakeHead;
+    private Vector3 cameraVelocity;
+
+    private void FixedUpdate()
+    {
+        transform.position = Vector3.SmoothDamp(transform.position, new Vector3(
+            snakeHead.position.x, snakeHead.position.y, transform.position.z), ref cameraVelocity,
+            cameraSmoothSpeed);
+    }
+}
